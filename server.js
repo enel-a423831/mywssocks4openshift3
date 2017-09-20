999ebb83511c41e554113fd1b9dc18defc44d862
Sandro Casali <a423831@e0529020.risorse.enel>
2017-09-20 11:48:11 +0200

    ...

--- server.js
+++ server.js
@@ -4,12 +4,11 @@
 /*************/
 
 var mywssocks = require('./modules/mywssocks')({
-    addr:    (process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"),
-    port:    (process.env.OPENSHIFT_NODEJS_PORT || 8080),
-    logTime:                                              true ,
-    debug:   (process.env.OPENSHIFT_NODEJS_PORT ? false : true),
-    dump:    (process.env.OPENSHIFT_NODEJS_PORT ? false : true),
-    extended:                                             true
+    addr:    (process.env.MYWSSOCKS_SERVICE_HOST || "127.0.0.1"),
+    port:    (process.env.MYWSSOCKS_SERVICE_PORT || 8080       ),
+    debug:   (process.env.MYWSSOCKS_SERVICE_PORT ? false : true),
+    dump:    (process.env.MYWSSOCKS_SERVICE_PORT ? false : true),
+    extended:                                              true
 });
 
 module.exports = mywssocks.app;
